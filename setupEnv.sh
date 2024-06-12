ac5c136161bab90ae61191758592caa9466e4350
Julius <julius@ostermann.cloud>
2024-06-12 08:53:49 +0200

    Add env setup bash

---
+++ setupEnv.sh
@@ -0,0 +1,17 @@
+# check if fogComputingVenv exists
+if [ ! -d "fogComputingVenv" ]; then
+    echo "Creating virtual environment fogComputingVenv"
+    python3 -m venv fogComputingVenv
+fi
+
+#activate the virtual environment
+source fogComputingVenv/bin/activate
+
+#check pip is installed
+if ! [ -x "$(command -v pip)" ]; then
+    echo "pip is not installed"
+    exit 1
+fi
+
+#install the required packages
+pip install -r requirements.txt
